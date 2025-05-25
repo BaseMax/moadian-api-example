c23eaa9e6bf0ca4b54450262208518185ca6c5ca
Max Base <MaxBaseCode@Gmail.Com>
2025-05-25 15:10:31 +0330

    Create nonce.php

---
+++ nonce.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ *
+ * @Author: Max Base
+ * @Date: 05/25/2025
+ * @Repository: https://github.com/BaseMax/moadian-api-example
+ *
+ **/
+
+// MIN is 10
+// DEFAULT is 30
+// MAX is 200
+$timeToLive = 200;
+
+$nonceUrl = "https://tp.tax.gov.ir/requestsmanager/api/v2/nonce?timeToLive=$timeToLive";
+$nonceResponse = file_get_contents($nonceUrl);
+if (!$nonceResponse) {
+    die("دریافت nonce با خطا مواجه شد.");
+}
+
+$nonceData = json_decode($nonceResponse, true);
+$nonce = $nonceData['nonce'] ?? null;
+$expDate = $nonceData['expDate'] ?? null;
+
+if (!$nonce) {
+    die("nonce یافت نشد.");
+}
+
+print $nonce;
